04fdcbf1e1a1f72657e0a312eabde5c0f5bcc1c2
jiya-patel2 <jiya.advaita@gmail.com>
2026-02-25 01:16:17 +0530

    Complete coffee machine

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -38,50 +38,52 @@ def insertCoin():
     rs_5 =  int(input("Enter number of 5 rupees coins :"))
     rs_10 =  int(input("Enter number of 10 rupees coins :"))
     rs_20 =  int(input("Enter number of 20 rupees coins :"))
-    coins = rs_1 + (rs_2) + (rs_10) + (rs_5) + (rs_20)
+    coins = rs_1 + (2 *rs_2) + (10*rs_10) + (5*rs_5) + (20*rs_20)
     return coins
 
 def checkResources():
-    for key,value in MENU :
-        if key == coffee_type :
-            key["ingredients"][value] >= resources
-
+    for ingredient in MENU[coffee_type]["ingredients"]:
+        if MENU[coffee_type]["ingredients"][ingredient] > resources[ingredient]:
+            print(f"Can't make {coffee_type}, insufficient {ingredient}")
 
 def serveOrder():
-    for key in resources:
-        insertCoin()
+    checkResources()
+    for key in MENU:
+        coins = insertCoin()
         if coins < MENU[key]["cost"]:
+            print(f"Not sufficient money\nYour refund is {coins}")
             break
         else :
             resources["money"] += MENU[key]["cost"]
             change = coins - MENU[key]["cost"]
             print(f"Here is Rs.{change} in change.")
-            print(f"Here is your {key} ☕️. Enjoy!")
+            print(f"Here is your {coffee_type} ☕️. Enjoy!")
+            for ingredient in MENU[coffee_type]["ingredients"]:
+                resources[ingredient] -= MENU[coffee_type]["ingredients"][ingredient] 
+            break
+def report():
+    # if customer asks for report
+    print(f"Water : {resources["water"]}ml")
+    print(f"Milk : {resources["milk"]}ml")
+    print(f"Coffee : {resources["coffee"]}g : ")
+    print(f"Money : Rs. {resources["money"]}")
+
 
 # Keeping machine On for all the customers
 machine = "on"
 while machine == "on" :
     # ask coffee type
     coffee_type = input("What would you like? (espresso/latte/cappuccino):")
-    coins = 0
     if coffee_type == "off":
         machine == "off"
         break
-    elif coffee_type == "espresso":
+    elif coffee_type == "espresso" or coffee_type == "latte" or coffee_type == "capuccino":
        serveOrder()
        print("\n")
-    elif coffee_type == "latte":
-        serveOrder()
-        print("\n")
-    elif coffee_type == "cappuccino":
-        serveOrder()
-        print("\n")
     elif coffee_type == "report":
-        # if customer asks for report
-        print(f"Water : {resources["water"]}ml")
-        print(f"Milk : {resources["milk"]}ml")
-        print(f"Coffee : {resources["coffee"]}g : ")
-        print(f"Money : Rs. {resources["money"]}")
+        report()
+    else:
+        print("Enter please recheck the Menu")
 
     
 
